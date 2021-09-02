1ba01b06bee7d1b0dc79b90b5b69f35b76b22741
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-09-02 18:53:49 -0300

    temp: added controller and service

---
+++ src/controllers/client/hotel.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+//import * as service from "@/services/client/hotel";
+
+export async function get(req: Request, res: Response) {
+  //const hotelInfo = await service.getEventInfo();
+  //res.send(hotelInfo);
+}
