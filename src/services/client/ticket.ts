fcdd68c3076d08f768eef57abcaae28bf453e067
Mateus Ribeiro <mateusrfon@gmail.com>
2021-09-03 17:05:25 -0300

    refactor: get prices as obj

--- src/services/client/ticket.ts
+++ src/services/client/ticket.ts
@@ -4,4 +4,9 @@ import TicketPrice from "@/entities/TicketPrice";
 
 export async function getPrices() {
   const prices = await TicketPrice.getPrices();
+  let pricesObj = {};
+  prices.forEach((item) => {
+    pricesObj = { ...pricesObj, [item.name]: item.price };
+  });
+  return pricesObj;
 }
