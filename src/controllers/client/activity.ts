546b9d8472ac1d8fee5ae618c1f86300f787172b
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-09-09 21:28:24 -0300

    feat: user now can reservate seat at activity

--- src/controllers/client/activity.ts
+++ src/controllers/client/activity.ts
@@ -2,7 +2,22 @@ import { Request, Response } from "express";
 
 import * as service from "@/services/client/activity";
 
-export async function get(req: Request, res: Response) {
+export async function getActivitiesDaysList(req: Request, res: Response) {
   const activities = await service.getActivitiesDaysList();
   res.send(activities);
+}
+
+export async function saveActivityUserReservation(req: Request, res: Response) {
+  const userId: number = req.user.id;
+  const activityId: number = req.body.activityId;
+  const userActivity = await service.saveActivityUserReservation(
+    userId,
+    activityId
+  );
+  console.log(userActivity);
+  if (!userActivity) {
+    console.log("entrou no null");
+    return res.sendStatus(403);
+  }
+  return res.send(userActivity);
 }
--- src/services/client/activity.ts
+++ src/services/client/activity.ts
@@ -1,7 +1,31 @@
+import Activity from "@/entities/Activity";
 import EventDay from "@/entities/EventDay";
+import User from "@/entities/User";
+import UserActivities from "@/entities/UserActivities";
 
 export async function getActivitiesDaysList() {
-  const activitiesDaysList = await EventDay.find();
+  const activitiesDaysList = await EventDay.createQueryBuilder("event_day")
+    .leftJoinAndSelect("event_day.activities", "activity")
+    .leftJoinAndSelect("activity.activityPlace", "place")
+    .leftJoinAndSelect("activity.users", "user")
+    .orderBy("activity.id", "ASC")
+    .getMany();
 
   return activitiesDaysList;
+}
+
+export async function saveActivityUserReservation(
+  userId: number,
+  activityId: number
+) {
+  const user = await User.findOne({ where: { id: userId } });
+  const activity = await Activity.findOne({ where: { id: activityId } });
+  if (activity.totalSeats - activity.users.length < 1) {
+    return null;
+  }
+  const UserActivity = UserActivities.create();
+  UserActivity.user = user;
+  UserActivity.activity = activity;
+  await UserActivity.save();
+  return UserActivity;
 }
